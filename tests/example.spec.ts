263ce8e71cb43e8f5b24a008dcea86825c5275c7
Minh Nguyen <minxomango@gmail.com>
2025-12-28 22:52:03 +0100

    feat(example): edit example test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.describe('HOMEPAGE', () => {
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  test('Verify page title', async ({ page }) => {
+    await page.goto('https://material.playwrightvn.com/');
+    await expect(page).toHaveTitle(/Tài liệu học automation test/);
+  });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  test('get started link', async ({ page }) => {
+    await page.goto('https://material.playwrightvn.com/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Bài học 1: Register Page (có đủ các element)' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+    // Expects page to have a heading with the name of Installation.
+    await expect(page.getByRole('heading', { name: 'User Registration' })).toBeVisible();
+  });
+
+})
